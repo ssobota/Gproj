942e1d0d7d156d30915925aeea033c4e2e220ca5
ssobot <sz.sobotkiewicz@gmail.com>
2020-11-05 15:53:12 +0100

    add main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public String text;
+    private int num;
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println();
+    }
+}
+
+
+
