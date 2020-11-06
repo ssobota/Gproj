73e73dab8b8b6152378cae78cc93f71d266b3621
ssobot <sz.sobotkiewicz@gmail.com>
2020-11-06 10:58:59 +0100

    Edit Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("start");
+
         int a = 0;
         a+=1;
         System.out.println(a);
